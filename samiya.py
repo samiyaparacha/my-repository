c19c5dc0bd2f73303dde5a3bcdd46ff3906e2d06
samiyaparacha <160571069+samiyaparacha@users.noreply.github.com>
2025-04-18 15:53:07 +0500

    my first program

---
+++ samiya.py
@@ -0,0 +1,12 @@
+import random
+challenges = ["I am gratefull for my ALLAH PAK to pass my quater2 exsam "
+"try to work hard"
+"i make a shadule for my daily work"
+"work hard is a key of sucess"
+]
+
+daily-challenge ="work hard is a key of sucess"
+random.choice("work hard is a key of sucess")
+print("work hard is a key of sucess")
+
+print(daily-challenge)

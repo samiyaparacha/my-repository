62395f1f01430be4b64d18aca38d0ef7a21f4602
samiyaparacha <160571069+samiyaparacha@users.noreply.github.com>
2025-04-19 21:28:32 +0500

    how old are they

---
+++ aasingment4howold are they.py
@@ -0,0 +1,13 @@
+def main():
+    anton:int = 21 #anton asge is given as 21 years old
+    beth:int = 6+anton #beth is 6 years older then anton,so add 6 to anton's age to get beth's
+    chen:int = 20+beth #chen is 20 years older then Beth,so add 20 to beth's age to get chen's
+    drew:int =chen_anton #drew is as old as Chen's age ples Anton's age, so add them together
+    ethan:int = chen #ethen is the same age as chen,so set Ethan's age is equal to Chen's
+    print("Anton is"+str(anton))
+    print("Beth is"+str(beth))
+    print("Chen is"+str(chen))
+    print("Drew is"+str(drew))
+    print("Ethen is"+str(ethen))
+     if_name_=='_main_':
+        main()

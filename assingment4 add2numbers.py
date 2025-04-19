6872787a4aa2cdcce05bbebe844b3560abe88899
samiyaparacha <160571069+samiyaparacha@users.noreply.github.com>
2025-04-19 21:15:48 +0500

    add 2 numbers

---
+++ assingment4 add2numbers.py
@@ -0,0 +1,9 @@
+def main():
+    print("this program add two numbers")
+    num1 : str = input("Enter first number:")
+    num1 : int = int(num1)
+    num2 : str = input("Enter second number:")
+    num2 : int = int(num2)
+    total : int = num1+num2print ("the total is"+str(total)+".")
+    if_name_=='_main_':
+        main()

c0af431d6fb07b0e80fee85a1c209beeeea30615
samiyaparacha <160571069+samiyaparacha@users.noreply.github.com>
2025-04-19 21:17:26 +0500

    perimeter program

---
+++ assingment4triangle perimeter.py
@@ -0,0 +1,8 @@
+def main():
+    side1: float = float(input("what is the length of side 1 ?"))
+    side2: float = float(input("what is the length of side 2 ?"))
+    side3: float = float(input("what is the length of side 3 ?"))
+    print("the perimeter of the triangle is "+ str(side1 + side2 + side3))
+    if_name_=='_main_'
+    main()
+    

24a0c9b6dff13a2053e3991d06b4f7575f2225a5
samiyaparacha <160571069+samiyaparacha@users.noreply.github.com>
2025-07-05 21:51:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    print("Asalamoalikum world!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+from dotenv import load-dotenv
+import os
+load-dotenv()
+gemini-api-key = os.getenv("GEMINI-API-KEY")
+
+
+
+
+
+
+
+
+
+
+
+
